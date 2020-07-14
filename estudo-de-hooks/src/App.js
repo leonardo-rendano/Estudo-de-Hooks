9c8c336295c7aacb7cebc7868123dae1f56df0f1
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-14 02:46:43 -0300

    Desenvolvimento da aplicação utilizando o Hooks

--- estudo-de-hooks/src/App.js
+++ estudo-de-hooks/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [ numero, setNumero ] = useState()
+  const [ segundoNumero, setSegundoNumero ] = useState()
+  const [ resultado, setResultado ] = useState()
+
+  const somar = () => {
+    const numeroFormatado = parseInt(numero)
+    const numeroFormatado2 = parseInt(segundoNumero)
+
+    setResultado( numeroFormatado + numeroFormatado2 )
+  }
+
   return(
     <div>
-      <h1>Hello World!</h1>
+      Primeiro número: <br />
+      <input type="text" value={numero} onChange={e => setNumero(e.target.value)} /> <br />
+      Segundo número: <br />
+
+      <input type="text" value={segundoNumero} onChange={e => setSegundoNumero(e.target.value)} /> <br />
+
+      Resultado: <br />
+      <input type="text" value={resultado} /> <br />
+
+      <button onClick={somar} > Somar </button>
     </div>
   );
 }
